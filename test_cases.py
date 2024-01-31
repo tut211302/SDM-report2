962b5db88d1a8192aae8a1d0b1447bb025bdd6d5
tut211302 <153628638+tut211302@users.noreply.github.com>
2024-01-31 19:14:11 +0900

    Update test_cases.py

--- test_cases.py
+++ test_cases.py
@@ -11,11 +11,47 @@ class TestCalc (unittest.TestCase):
                 self.assertEqual (21, calc(3,7))
 
         def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+                self.assertEqual (999, calc(1,999))
 
         def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+                self.assertEqual (-1, calc(0,999))
 
         def test_sample4 (self):
+                self.assertEqual (-1, calc(1,1000))
+
+        def test_sample5 (self):
+                self.assertEqual (-1, calc(0,1000))
+
+        def test_sample6 (self):
+                self.assertEqual (-1, calc(1,999.9))
+
+        def test_sample7 (self):
                 self.assertEqual (-1, calc(0.1,999))
+
+        def test_sample8 (self):
+                self.assertEqual (-1, calc(0.1,999.9))
+
+        def test_sample9 (self):
+                self.assertEqual (-1, calc(-999,1))
+
+        def test_sample10 (self):
+                self.assertEqual (-1, calc(-999,-1))
+
+        def test_sample11 (self):
+                self.assertEqual (-1, calc(1+1j,999))
+
+        def test_sample12 (self):
+                self.assertEqual (-1, calc(1,600+400j))
+
+        def test_sample13 (self):
+                self.assertEqual (-1, calc(1+1j,600+400j))
+
+        def test_sample14 (self):
+                self.assertEqual (-1, calc('a',999))
+
+        def test_sample15 (self):
+                self.assertEqual (-1, calc(1,'b'))
+
+        def test_sample16 (self):
+                self.assertEqual (-1, calc('a','b'))
 
